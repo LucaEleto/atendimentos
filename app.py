f1cd4a000fdcd925cc6312cec4ec14ff55c1f9fe
Luca Eleto <lucavilarino321@gmail.com>
2025-07-31 09:40:29 -0300

    Update app.py

--- app.py
+++ app.py
@@ -164,9 +164,7 @@ def tela_atendimentos():
             st.success('Atendimento registrado com sucesso!')
             st.session_state.cliente_selecionado = None
             st.session_state.clientes_filtrados = []
-            # NÃO limpe observacao_cliente aqui!
-            if 'radio_cliente' in st.session_state:
-                del st.session_state.radio_cliente
+            
 
 def paniel_admin():
     st.title('Painel de Administração')
